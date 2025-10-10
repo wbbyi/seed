6eb1e7ce910abb52f5943d1b30a54bbfa510ad2d
nann <nann@123.com>
2025-10-10 15:30:05 +0800

    添加数据库操作函数

---
+++ uniCloud-alipay/cloudfunctions/saveHistory/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const db = uniCloud.database();
+
+exports.main = async (event, context) => {
+  const { action, collection, data, where } = event;
+  const openid = context.OPENID; // 获取用户唯一id
+
+  const collectionRef = db.collection(collection);
+
+  switch (action) {
+    case 'add':
+      // 添加数据，并附上openid
+      return await collectionRef.add({
+        ...data,
+        openid,
+        create_time: Date.now()
+      });
+
+    case 'get':
+      // 获取该用户的全部数据
+      return await collectionRef.where({
+        openid
+      }).get();
+
+    case 'update':
+      // 更新当前用户的指定数据
+      return await collectionRef.where({
+        openid,
+        ...where
+      }).update(data);
+
+    case 'delete':
+      // 删除指定数据
+      return await collectionRef.where({
+        openid,
+        ...where
+      }).remove();
+
+    case 'get_openid':
+      // 单独返回openid
+      return { openid };
+
+    default:
+      return { msg: '未知操作类型' };
+  }
+};
